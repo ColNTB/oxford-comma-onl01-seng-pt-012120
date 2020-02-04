d94df0c87fb990ac203dc7fee084c9c89d925d8f
Nathan Bohn <nathantbohn@gmail.com>
2020-02-04 22:03:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,5 @@ def oxford_comma(array)
        return array.join 
      elsif array.size == 2 
         return array.join(" and ")
-     elsif array.size == 3
-           i = 0; 
-           while i<(array.size)
-              array[i] = ","
-           end 
-           
-           return array 
       end 
 end
