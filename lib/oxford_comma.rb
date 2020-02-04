adff5244fa37600cd504ec03295b75071868c512
Nathan Bohn <nathantbohn@gmail.com>
2020-02-04 22:18:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+     if (array.size<2)
+       return array.join 
+     elsif array.size == 2 
+        return array.join(" and ")
+     elsif array.size == 3 
+         array[-1].insert(0, "and " )
+         array.join(", ")
+     else 
+         array[-1].insert(0, "and ")
+         array.join(", ")
+      end 
+    
 end
