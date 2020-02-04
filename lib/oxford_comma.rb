7215cb3129a74b2a79f691b943addca3d77da4c7
Nathan Bohn <nathantbohn@gmail.com>
2020-02-04 22:16:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,12 @@ def oxford_comma(array)
        return array.join 
      elsif array.size == 2 
         return array.join(" and ")
-      elsif array.size == 3 
+     elsif array.size == 3 
          array[-1].insert(0, "and" )
          array.join(", ")
+     else 
+         array[-1].insert(0, "and ")
+         array.join(", ")
       end 
+    
 end
